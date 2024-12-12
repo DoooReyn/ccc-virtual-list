5d78461e6bebfb867ad57182f97c0792f182fc61
Reyn <jl88744653@gmail.com>
2024-12-12 21:01:14 +0800

    基础版本完成

--- assets/src/VList.ts
+++ assets/src/VList.ts
@@ -17,7 +17,9 @@ import {
     Size,
     Label,
     Sprite,
+    Rect,
 } from "cc";
+import VItem from "./VItem";
 const { ccclass, property } = _decorator;
 
 const __TEST__ = true;
@@ -36,8 +38,6 @@ export const LIST_LAYOUT = Enum({
     SINGLE_LINE: 0,
     /** 多行多列 */
     MULTI_LINE: 1,
-    /** 自由增长 */
-    AUTO_GROW: 2,
 });
 
 const LAST_POS = new Vec2();
@@ -61,6 +61,73 @@ class VirtualItem {
     public x: number = 0;
     /** 子项纵坐标 */
     public y: number = 0;
+    /** 虚拟列表 */
+    public list: VList = null;
+    private _rect: Rect = new Rect();
+    /**
+     * 检查边界
+     * @param bounds 容器边界
+     */
+    public checkBounds(bounds: Rect) {
+        this._rect.set(this.x + this.list.container.position.x, this.y + this.list.container.position.y, this.w, this.h);
+        const intersects = this._rect.intersects(bounds);
+        intersects ? this.onShow() : this.onHide();
+        return intersects;
+    }
+    protected onShow() {
+        // @ts-ignore
+        this.list.onItemShow(this);
+    }
+    protected onHide() {
+        // @ts-ignore
+        this.list.onItemHide(this);
+    }
+    protected onRecycle() {
+        this.i = this.w = this.h = this.x = this.y = 0;
+        this.list = null;
+    }
+    public get position() {
+        if (this.list.horizontal) {
+            return new Vec3(this.x + this.w / 2, this.y, 0);
+        } else {
+            return new Vec3(this.x, this.y + this.h / 2, 0);
+        }
+    }
+}
+
+class VirtualItemFactory {
+    private static _inst: VirtualItemFactory = null;
+    public static get inst() {
+        return (this._inst ??= new VirtualItemFactory());
+    }
+    private _pool: VirtualItem[] = [];
+    public get(): VirtualItem {
+        if (this._pool.length > 0) {
+            return this._pool.pop();
+        }
+        return new VirtualItem();
+    }
+    public put(item: VirtualItem) {
+        this._pool.push(item);
+    }
+}
+
+class RealItemFactory {
+    private _template: Node;
+    private _pool: Node[] = [];
+    public set template(t: Node) {
+        this._template = t;
+    }
+    public get(): Node {
+        if (this._pool.length > 0) {
+            return this._pool.pop();
+        }
+        return instantiate(this._template);
+    }
+    public put(item: Node) {
+        item.removeFromParent();
+        this._pool.push(item);
+    }
 }
 
 @ccclass("VList")
@@ -75,6 +142,8 @@ export class VList extends Component {
     private _dataSource: any[] | null;
     private _startPos: Vec3 = null;
     protected _vitems: VirtualItem[] = [];
+    private _dirty: boolean = false;
+    protected _itemPool: RealItemFactory = new RealItemFactory();
 
     @property({ displayName: "滚动方向", type: LIST_DIRCTION, group: { id: "1", name: "基础参数", displayOrder: 1 } })
     protected $direction = LIST_DIRCTION.VERTICAL;
@@ -82,8 +151,10 @@ export class VList extends Component {
     protected $layout = LIST_LAYOUT.SINGLE_LINE;
     @property({ displayName: "子项间距", type: CCInteger, group: { id: "1", name: "基础参数", displayOrder: 3 } })
     protected $spacing = 0;
-    @property({ displayName: "惯性滚动", group: { id: "1", name: "基础参数", displayOrder: 4 } })
+    @property({ displayName: "滚动开关", group: { id: "1", name: "惯性", displayOrder: 1 } })
     protected $inertia = false;
+    @property({ displayName: "滚动倍速", group: { id: "1", name: "惯性", displayOrder: 2 } })
+    protected $speed = 1;
     @property({ displayName: "左", type: CCInteger, group: { id: "2", name: "边距", displayOrder: 1 } })
     protected $paddingLeft = 0;
     @property({ displayName: "右", type: CCInteger, group: { id: "2", name: "边距", displayOrder: 2 } })
@@ -94,8 +165,6 @@ export class VList extends Component {
     protected $paddingBottom = 0;
 
     protected onLoad(): void {
-        console.log("==load==");
-
         // 计算容器最小尺寸
         const { width, height } = this.node.getComponent(UITransform);
         const mw = width - this.$paddingLeft - this.$paddingRight;
@@ -111,13 +180,13 @@ export class VList extends Component {
         maskT.setContentSize(mw, mh);
         maskC.type = Mask.Type.GRAPHICS_RECT;
         maskC.inverted = false;
-        maskC.enabled = true;
+        maskC.enabled = false;
 
         // 创建容器
         const containerN = new Node("container");
         const containerC = containerN.addComponent(UITransform);
-        const anchor = this.$direction == LIST_DIRCTION.HORIZONTAL ? new Vec2(0, 0.5) : new Vec2(0.5, 1);
-        const pos = this.$direction == LIST_DIRCTION.HORIZONTAL ? new Vec3(-mw / 2, 0) : new Vec3(0, mh / 2);
+        const anchor = this.horizontal ? new Vec2(0, 0.5) : new Vec2(0.5, 1);
+        const pos = this.horizontal ? new Vec3(-mw / 2, 0) : new Vec3(0, mh / 2);
         containerC.setContentSize(mw, mh);
         containerC.setAnchorPoint(anchor);
         maskN.addChild(containerN);
@@ -128,7 +197,16 @@ export class VList extends Component {
 
         if (__TEST__) {
             containerN.addComponent(Graphics);
-            console.log(this);
+            (<any>window).vlist = this;
+        }
+
+        // 对象池
+        if (this.horizontal) {
+            const template = this.node.parent.getChildByName("HItem");
+            this._itemPool.template = template;
+        } else {
+            const template = this.node.parent.getChildByName("VItem");
+            this._itemPool.template = template;
         }
     }
 
@@ -147,33 +225,65 @@ export class VList extends Component {
     }
 
     protected start(): void {
-        console.log("==start==");
-        this.data = new Array(100).fill(0).map((_, i) => i + 1);
-        for (let i = 0; i < this._dataSource.length; i++) {
-            const item = this.appendItem();
-            const [width, height] = this.getItemSize(i);
-            item.setPosition(0, -height / 2 - height * i - this.$spacing * (i + 1));
-            item.getComponent(Sprite).color = new Color(Math.random() * 255, Math.random() * 255, Math.random() * 255);
-            item.getChildByName("Label").getComponent(Label).string = (i + 1).toString();
+        if (__TEST__) {
+            this.data = new Array(100).fill(0).map((_, i) => i + 1);
         }
     }
 
-    private appendItem() {
-        const template = this.node.parent.getChildByName("Item");
-        const item = instantiate(template);
+    /** 是否水平滚动 */
+    public get horizontal() {
+        return this.$direction == LIST_DIRCTION.HORIZONTAL;
+    }
+
+    /** 是否垂直滚动 */
+    public get vertical() {
+        return this.$direction == LIST_DIRCTION.VERTICAL;
+    }
+
+    /** 视图边界 */
+    protected get viewBounds() {
+        return this.node._uiProps.uiTransformComp.getBoundingBox();
+    }
+
+    /**
+     * 虚拟子项进入视野
+     * @param vitem 虚拟子项
+     */
+    protected onItemShow(vitem: VirtualItem) {
+        const item = this.getItemAt(vitem.i, true);
+        if (item) {
+            item.setPosition(vitem.position);
+        }
+    }
+
+    /**
+     * 虚拟子项移出视野
+     * @param vitem 虚拟子项
+     */
+    protected onItemHide(vitem: VirtualItem) {
+        const item = this.getItemAt(vitem.i, false);
+        if (item) {
+            this._itemPool.put(item);
+        }
+    }
+
+    /** 添加实体子项 */
+    private appendRealItem() {
+        const item = this._itemPool.get();
         item.active = true;
         this._container.addChild(item);
         return item;
     }
 
-    private drawBound() {
+    /** 绘制容器边界 */
+    private drawContainerBounds() {
         if (__TEST__) {
             const { width, height } = this._containerTransform.contentSize;
-            const pos = this.$direction == LIST_DIRCTION.HORIZONTAL ? new Vec3(0, -width / 2) : new Vec3(-width / 2, -height);
+            const pos = this.horizontal ? new Vec3(0, -width / 2) : new Vec3(-width / 2, -height);
             const containerG = this._container.getComponent(Graphics);
             containerG.clear();
             containerG.lineWidth = 2;
-            containerG.strokeColor = new Color(255, 0, 0);
+            containerG.strokeColor = new Color(255, 0, 255);
             containerG.fillColor = new Color(0, 0, 0, 50);
             containerG.fillRect(pos.x, pos.y, width, height);
             containerG.stroke();
@@ -195,7 +305,7 @@ export class VList extends Component {
     /**
      * 容器尺寸
      */
-    private get contentSize() {
+    public get contentSize() {
         return this._containerTransform.contentSize;
     }
 
@@ -206,13 +316,14 @@ export class VList extends Component {
     private onTouchMove(e: EventTouch) {
         this.stopScroll();
         const delta = e.getDelta();
-        if (this.$direction == LIST_DIRCTION.HORIZONTAL) {
+        if (this.horizontal) {
             LAST_POS.x += delta.x;
             this._container.setPosition(this._container.position.x + delta.x, this._container.position.y);
         } else {
             LAST_POS.y += delta.y;
             this._container.setPosition(this._container.position.x, this._container.position.y + delta.y);
         }
+        this._dirty = true;
     }
 
     /**
@@ -227,8 +338,8 @@ export class VList extends Component {
             LEAVE_POS.x = e.getLocationX();
             LEAVE_POS.y = e.getLocationY();
             const delta = LEAVE_AT - DROP_AT;
-            if (this.$direction == LIST_DIRCTION.HORIZONTAL && Math.abs(LEAVE_POS.x - DROP_POS.x) < SCROLL_SPAN) return;
-            if (this.$direction == LIST_DIRCTION.VERTICAL && Math.abs(LEAVE_POS.y - DROP_POS.y) < SCROLL_SPAN) return;
+            if (this.horizontal && Math.abs(LEAVE_POS.x - DROP_POS.x) < SCROLL_SPAN) return;
+            if (this.vertical && Math.abs(LEAVE_POS.y - DROP_POS.y) < SCROLL_SPAN) return;
             if (delta > SCROLL_DELTA) return;
             this._scrolling = true;
             this._scrollDelta = delta / 1000;
@@ -242,22 +353,20 @@ export class VList extends Component {
      * @return
      */
     private checkBounce() {
-        if (this.$direction == LIST_DIRCTION.HORIZONTAL) {
-            if (this._container.position.x <= this._startPos.x) {
-                this.scrollTo(this._startPos, AUTO_ANIMATE_DELTA);
+        if (this.horizontal) {
+            if (this._container.position.x > this._startPos.x) {
+                this.scrollToStart(AUTO_ANIMATE_DELTA);
                 return true;
-            } else if (this._container.position.x >= this._containerTransform.width - this.minWidth * 1.5) {
-                const pos = new Vec3(this._containerTransform.width - this.minWidth * 1.5, this._container.position.y);
-                this.scrollTo(pos, AUTO_ANIMATE_DELTA);
+            } else if (this._container.position.x < -this._containerTransform.width + this.minWidth / 2) {
+                this.scrollToEnd(AUTO_ANIMATE_DELTA);
                 return true;
             }
         } else {
             if (this._container.position.y < this._startPos.y) {
-                this.scrollTo(this._startPos, AUTO_ANIMATE_DELTA);
+                this.scrollToStart(AUTO_ANIMATE_DELTA);
                 return true;
             } else if (this._container.position.y > this._containerTransform.height - this.minHeight / 2) {
-                const pos = new Vec3(this._container.position.x, this._containerTransform.height - this.minHeight / 2);
-                this.scrollTo(pos, AUTO_ANIMATE_DELTA);
+                this.scrollToEnd(AUTO_ANIMATE_DELTA);
                 return true;
             }
         }
@@ -289,24 +398,53 @@ export class VList extends Component {
      * @param data 数据源
      */
     public set data(data: any[]) {
-        if (data == null) {
-            this.clear();
-            return;
-        }
+        // 更新数据源
         this._dataSource = data;
+
+        // 清理虚拟子项
+        for (let i = 0, ritem: Node; i < this._vitems.length; i++) {
+            ritem = this.getItemAt(i, false);
+            if (ritem) this._itemPool.put(ritem);
+            VirtualItemFactory.inst.put(this._vitems[i]);
+        }
+        this._vitems.length = 0;
+
+        // 添加虚拟子项
+        const hor = this.horizontal;
+        if (data && data.length > 0) {
+            let item: VirtualItem;
+            let bounds = this.viewBounds;
+            for (let i = 0; i < this._dataSource.length; i++) {
+                const [width, height] = this.getItemSize(i);
+                item = VirtualItemFactory.inst.get();
+                item.list = this;
+                item.i = i;
+                item.x = hor ? (width + this.$spacing) * (i + 1) - width : 0;
+                item.y = hor ? 0 : -(height + this.$spacing) * (i + 1);
+                item.w = width;
+                item.h = height;
+                item.checkBounds(bounds);
+                this._vitems.push(item);
+            }
+        }
+
+        // 更新容器
         this._containerTransform.setContentSize(this.calculateSize());
-        this.drawBound();
+        this._container.setPosition(this._startPos);
+        this.drawContainerBounds();
+        this.checkVirtualBounds();
     }
 
     /**
      * 清空数据源
      */
     public clear() {
-        this._dataSource = null;
-        const children = this._container.children;
-        for (let i = children.length - 1; i >= 0; i--) {
-            // @todo 回收
-            // children[i].destroy();
+        const count = this._dataSource.length;
+        for (let i = count - 1, ritem: Node; i >= 0; i--) {
+            ritem = this.getItemAt(i, false);
+            if (ritem) {
+                this._itemPool.put(ritem);
+            }
         }
     }
 
@@ -324,7 +462,11 @@ export class VList extends Component {
      */
     public getItemSize(index: number): [number, number] {
         // @todo
-        return [1024, 120];
+        if (this.horizontal) {
+            return [180, 120];
+        } else {
+            return [1024, 120];
+        }
     }
 
     /**
@@ -333,8 +475,18 @@ export class VList extends Component {
      * @returns
      */
     public getItemAt(index: number, force: boolean = false): Node | null {
-        // @todo
-        return null;
+        const name = `item_${index}`;
+        let item = this._container.getChildByName(name);
+        if (!item && force) {
+            item = this.appendRealItem();
+            item.getChildByName("Label").getComponent(Label).string = (index + 1).toString();
+        }
+        if (item) {
+            item.name = name;
+            // const height = this.getItemSize(index)[1];
+            // item.setPosition(0, -height / 2 - height * index - this.$spacing * (index + 1));
+        }
+        return item;
     }
 
     /**
@@ -362,7 +514,7 @@ export class VList extends Component {
         let width = 0,
             height = 0,
             size: [number, number];
-        let hor = this.$direction == LIST_DIRCTION.HORIZONTAL;
+        let hor = this.horizontal;
         let spacing = this.$spacing;
         let count = this.count;
         for (let i = 0; i < count; i++) {
@@ -400,7 +552,18 @@ export class VList extends Component {
      */
     public scrollTo(position: Vec3, delta: number = 0) {
         this.stopScroll();
-        tween(this._container).to(delta, { position: position }).start();
+        tween(this._container)
+            .to(delta, { position: position }, { onUpdate: () => this.checkVirtualBounds() })
+            .call(this.updateBounds, this)
+            .start();
+    }
+
+    /**
+     * 更新边界与回弹
+     */
+    private updateBounds() {
+        this.checkBounce();
+        this.checkVirtualBounds();
     }
 
     /**
@@ -409,8 +572,44 @@ export class VList extends Component {
      * @param delta 动画时间
      */
     public scrollToIndex(index: number, delta: number = 0) {
-        // @todo
-        const pos = this._container.position;
+        const vitem = this._vitems[index];
+        if (vitem) {
+            const pos = vitem.position.clone();
+            if (this.horizontal) {
+                pos.x = -pos.x - this._minWidth / 2 + this.$spacing / 2 + vitem.w / 2;
+                pos.x = Math.max(pos.x, -this.contentSize.width + this.minWidth / 2);
+            } else {
+                pos.y = -pos.y - vitem.h / 2 - this.$spacing / 2 + this._minHeight / 2;
+                pos.y = Math.min(pos.y, this.contentSize.height - this._minHeight / 2);
+            }
+            // @test
+            __TEST__ && console.log("==scrollTo==", index, pos.toString());
+            this.scrollTo(pos, delta);
+        }
+    }
+
+    /**
+     * 滚动到开始处
+     * @param delta 动画时间
+     */
+    public scrollToStart(delta: number = 0) {
+        if (this.horizontal) {
+            this.scrollToLeft(delta);
+        } else {
+            this.scrollToTop(delta);
+        }
+    }
+
+    /**
+     * 滚动到结束处
+     * @param delta 动画时间
+     */
+    public scrollToEnd(delta: number = 0) {
+        if (this.horizontal) {
+            this.scrollToRight(delta);
+        } else {
+            this.scrollToBottom(delta);
+        }
     }
 
     /**
@@ -418,7 +617,7 @@ export class VList extends Component {
      * @param delta 动画时间
      */
     public scrollToLeft(delta: number = 0) {
-        if (this.$direction == LIST_DIRCTION.HORIZONTAL) {
+        if (this.horizontal) {
             this.scrollToIndex(0, delta);
         }
     }
@@ -428,7 +627,7 @@ export class VList extends Component {
      * @param delta 动画时间
      */
     public scrollToRight(delta: number = 0) {
-        if (this.$direction == LIST_DIRCTION.HORIZONTAL) {
+        if (this.horizontal) {
             this.scrollToIndex(this.count - 1, delta);
         }
     }
@@ -438,7 +637,7 @@ export class VList extends Component {
      * @param delta 动画时间
      */
     public scrollToTop(delta: number = 0) {
-        if (this.$direction == LIST_DIRCTION.VERTICAL) {
+        if (this.vertical) {
             this.scrollToIndex(0, delta);
         }
     }
@@ -448,8 +647,19 @@ export class VList extends Component {
      * @param delta 动画时间
      */
     public scrollToBottom(delta: number = 0) {
-        if (this.$direction == LIST_DIRCTION.VERTICAL) {
+        if (this.vertical) {
             this.scrollToIndex(this.count - 1, delta);
+        }
+    }
+
+    /**
+     * 检查虚拟子项的边界
+     */
+    private checkVirtualBounds() {
+        const bounds = this.viewBounds;
+        for (let i = 0, item: VirtualItem; i < this._vitems.length; i++) {
+            item = this._vitems[i];
+            item.checkBounds(bounds);
         }
     }
 
@@ -461,15 +671,20 @@ export class VList extends Component {
                 return;
             }
             if (this.checkBounce()) return;
+            this._dirty = true;
             this._scrollDelta -= dt * 0.1;
-            const speedX = this._scrollOffset.x * this._scrollDelta * 2;
-            const speedY = this._scrollOffset.y * this._scrollDelta * 2;
+            const speedX = this._scrollOffset.x * this._scrollDelta * this.$speed;
+            const speedY = this._scrollOffset.y * this._scrollDelta * this.$speed;
             const pos = this._container.position;
-            if (this.$direction == LIST_DIRCTION.HORIZONTAL) {
+            if (this.horizontal) {
                 this._container.setPosition(pos.x + speedX, pos.y);
             } else {
                 this._container.setPosition(pos.x, pos.y + speedY);
             }
+        }
+        if (this._dirty) {
+            this._dirty = false;
+            this.checkVirtualBounds();
         }
     }
 }
